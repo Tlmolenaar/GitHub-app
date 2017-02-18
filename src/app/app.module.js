6d84629b51b15f25a54de56bd4bc5600358792a4
Thomas Molenaar <tlmolenaar@hotmail.com>
2017-02-18 13:31:29 +0000

    finished application

--- src/app/app.module.js
+++ src/app/app.module.js
@@ -10,14 +10,19 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 };
 var core_1 = require('@angular/core');
 var platform_browser_1 = require('@angular/platform-browser');
+var http_1 = require('@angular/http');
+var forms_1 = require('@angular/forms');
 var app_component_1 = require('./app.component');
+var profile_component_1 = require('./components/profile.component');
+var github_service_1 = require('./services/github.service');
 var AppModule = (function () {
     function AppModule() {
     }
     AppModule = __decorate([
         core_1.NgModule({
-            imports: [platform_browser_1.BrowserModule],
-            declarations: [app_component_1.AppComponent],
+            imports: [platform_browser_1.BrowserModule, http_1.HttpModule, forms_1.FormsModule],
+            declarations: [app_component_1.AppComponent, profile_component_1.ProfileComponent],
+            providers: [github_service_1.GithubService],
             bootstrap: [app_component_1.AppComponent]
         }), 
         __metadata('design:paramtypes', [])
